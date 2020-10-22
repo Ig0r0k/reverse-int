94c38d7f788efe2bcdf8f6c805349b592ded43ed
Ihor Holysh <58191199+Ig0r0k@users.noreply.github.com>
2020-10-22 10:59:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let result;
+  if (n < 0) {
+    n *= -1;
+  }
+  result = String(n).split('').reverse().join('');
+  return +result;
 }
